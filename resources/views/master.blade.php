232bce28145567da69f17749d0dbe696eb2fde79
sajanthakur02 <thakursajan758@gmail.com>
2023-09-12 15:55:28 +0530

    login page

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,7 +8,9 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}}
 </body>
 <script>
     $(document).ready(function () {
